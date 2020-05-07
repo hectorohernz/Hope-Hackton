3291f6abd530ffc5fa07b93ff5e78cda78cc1bd3
Hector Hernandez <hectorohernz@gmail.com>
2020-05-06 22:13:07 -0400

    add resources

--- server/index.js
+++ server/index.js
@@ -26,7 +26,24 @@ app.get("/feed",(req,res) => {
 app.get("/resources", (req,res) => {
     res.render("resources");
 }); 
-app.get('/resources/shelter', (req,res) => {
+
+app.get("/resources/shelter", (req,res) => {
+    res.render("shelter");
+});
+
+app.get("/resources/foodbanks", (req,res) => {
+    res.render("food-banks");
+});
+
+app.get("/resources/mentalhealth", (req,res) => {
+    res.render("mental");
+});
+
+app.get("/resources/clothes", (req,res) => {
+    res.render("clothing");
+});
+
+app.get('/resources/api/shelter', (req,res) => {
     let source = 'welfare';
     let lat = 40.752655;
     let lon = -73.977295;
@@ -41,7 +58,55 @@ app.get('/resources/shelter', (req,res) => {
         }      
     });
 });
+app.get('/resources/api/foodbank', (req,res) => {
+    let source = 'foodbank';
+    let lat = 40.752655;
+    let lon = -73.977295;
+    let url = `https://api.tomtom.com/search/2/search/${source}.json?key=${apiKey}&lat=40.752655&lon=-73.977295`;
+    request(url, (error,response,body) => {
+        if(error){
+           console.log(url);
+        } else{
+            let info = JSON.parse(body);
+            const results = info.results;
+            res.send(results);
+        }      
+    });
+});
+app.get('/resources/api/mentalhealth', (req,res) => {
+    let source = 'mental-health';
+    let lat = 40.752655;
+    let lon = -73.977295;
+    let url = `https://api.tomtom.com/search/2/search/${source}.json?key=${apiKey}&lat=40.752655&lon=-73.977295`;
+    request(url, (error,response,body) => {
+        if(error){
+           console.log(url);
+        } else{
+            let info = JSON.parse(body);
+            const results = info.results;
+            res.send(results);
+        }      
+    });
+});
+app.get('/resources/api/clothes', (req,res) => {
+    let source = 'clothing-donation';
+    let lat = 40.752655;
+    let lon = -73.977295;
+    let url = `https://api.tomtom.com/search/2/search/${source}.json?key=${apiKey}&lat=40.752655&lon=-73.977295`;
+    request(url, (error,response,body) => {
+        if(error){
+           console.log(url);
+        } else{
+            let info = JSON.parse(body);
+            const results = info.results;
+            res.send(results);
+        }      
+    });
+});
 
+app.get('*', function(req, res){
+    res.render('404');
+  });
 const port = process.env.PORT || 3000;
 app.listen (port, () => {
     console.log(`Server Is On http://localhost:${port}`);
